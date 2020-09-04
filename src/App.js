d1a8e9d9d0bf909ec04813b2d5f5767d23fb507a
unknown <rohit.raj.bhandari07@gmail.com>
2020-09-04 14:27:15 +0300

    store,reducer and navbar created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-
 import "./App.css";
+import { Switch, Route, NavLink, Redirect } from "react-router-dom";
+import PokemonList from "./containers/PokemonList";
+import Pokemon from "./containers/Pokemon";
+import SearchAppBar from "./containers/SearchAppBar";
 
 function App() {
   return (
     <div className='App'>
-      <h1>POKEMON-DESKBOARD</h1>
+      <SearchAppBar />
+      <Switch>
+        <Route exact path={"/"} component={PokemonList} />
+        <Route exact path={"/Pokemon/:pokemon"} component={Pokemon} />
+        <Redirect to={"/"} />
+      </Switch>
     </div>
   );
 }
---
+++ src/containers/Pokemon.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pokemon = (props) => {
+  console.log("props", props);
+  return <div>pokemon</div>;
+};
+export default Pokemon;
