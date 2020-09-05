a73c0d0fb9b7feff71889cc9c9150fc834bbfe4d
unknown <rohit.raj.bhandari07@gmail.com>
2020-09-05 22:42:23 +0300

    react-pagination installed

--- src/containers/PokemonList.js
+++ src/containers/PokemonList.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import _ from "lodash";
 import GetPokemonList from "../actions/PokemonListAction";
 import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 
 const PokemonList = (props) => {
   const [search, setSearch] = useState("");
@@ -19,6 +20,9 @@ const PokemonList = (props) => {
   };
 
   const ShowData = () => {
+    if (pokemonList.loading) {
+      return <p>loading...</p>;
+    }
     if (!_.isEmpty(pokemonList.data)) {
       return (
         <div className={"list-wrapper"}>
@@ -33,10 +37,6 @@ const PokemonList = (props) => {
           })}
         </div>
       );
-    }
-
-    if (pokemonList.loading) {
-      return <p>loading...</p>;
     }
 
     if (pokemonList.errMessage !== "") {
@@ -54,6 +54,15 @@ const PokemonList = (props) => {
         </button>
       </div>
       {ShowData()}
+      {!_.isEmpty(pokemonList.data) && (
+        <ReactPaginate
+          pageCount={Math.ceil(pokemonList.count / 20)}
+          pageRangeDisplayed={2}
+          marginPagesDisplayed={1}
+          onPageChange={(data) => FetchData(data.selected + 1)}
+          containerClassName={"pagination"}
+        />
+      )}
     </div>
   );
 };
