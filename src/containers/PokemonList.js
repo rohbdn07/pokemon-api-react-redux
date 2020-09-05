15355e59767cd8aadb52b08b9cdb99284426a63f
unknown <rohit.raj.bhandari07@gmail.com>
2020-09-05 15:44:48 +0300

    styling the pokemonlist

--- src/containers/PokemonList.js
+++ src/containers/PokemonList.js
@@ -1,30 +1,13 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import _ from "lodash";
 import GetPokemonList from "../actions/PokemonActions";
 import { Link } from "react-router-dom";
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 350,
-    marginTop: 10,
-  },
-  media: {
-    height: 100,
-  },
-});
-
 const PokemonList = () => {
-  const classes = useStyles();
   const dispatch = useDispatch();
   const pokemonList = useSelector((state) => state.PokemonList);
 
@@ -38,31 +21,20 @@ const PokemonList = () => {
 
   const ShowData = () => {
     if (!_.isEmpty(pokemonList.data)) {
-      return pokemonList.data.map((item) => {
-        return (
-          <div className='flex'>
-            <Card className={classes.root}>
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image='/static/images/cards/contemplative-reptile.jpg'
-                  title='Contemplative Reptile'
-                />
-                <CardContent>
-                  <Typography gutterBottom variant='h5' component='h2'>
-                    {item.name}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Link to={`/pokemon/${item.name}`} size='small' color='primary'>
-                  Learn More
-                </Link>
-              </CardActions>
-            </Card>
-          </div>
-        );
-      });
+      return (
+        <div className={"list-wrapper"}>
+          {pokemonList.data.map((pokemon) => {
+            const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+            return (
+              <div className={"pokemon-item"}>
+                <p>{name}</p>
+
+                <Link to={`/pokemon/${name}`}>Show More</Link>
+              </div>
+            );
+          })}
+        </div>
+      );
     }
 
     if (pokemonList.loading) {
